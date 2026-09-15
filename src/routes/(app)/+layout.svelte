295b688eb6fa59f39ae2a3140c8f0760b2b0eb17
LeonNamowitz <leonnamowitzschool@gmail.com>
2026-09-15 22:19:30 +0200

    preserve source toggle on new search input for main route

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -81,9 +81,10 @@
 					goto(resolve("/"));
 					return;
 				}
-				const searchParams = new SvelteURLSearchParams({
-					query: encodeURIComponent(query),
-				});
+				const searchParams = new SvelteURLSearchParams(
+					isSearchPage ? page.url.searchParams : "",
+				);
+				searchParams.set("query", encodeURIComponent(query));
 				// Enable autofocus before running `goto` because on chromium
 				// the .focus() call won't work, even after a timeout.
 				// Using autofocus seems to work. Disables after goto runs.
